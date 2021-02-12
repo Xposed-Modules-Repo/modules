6148b172555d41d52b0e767ce062faf8c161c611
Riko Sakurauchi <lijiahao99131@gmail.com>
2021-02-12 16:15:36 +0800

    :bug: fix cache

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const uuid = require('uuid')
 const crypto = require('crypto')
 const path = require('path')
+const glob = require('glob')
+const md5 = require('uuid')
 
 const { fetchFromGithub } = require('./github-source')
 
@@ -376,4 +378,46 @@ exports.onPostBuild = async ({ graphql }) => {
     }
   }
   fs.writeFileSync(`${rootPath}/modules.json`, JSON.stringify(modules))
+
+  // Work around for caches on gh-pages
+  // https://github.com/gatsbyjs/gatsby/issues/15080#issuecomment-765338035
+  const publicPath = path.join(__dirname, 'public')
+  const hash = md5(Math.random().toString(36).substring(7))
+  const jsonFiles = glob.sync(`${publicPath}/page-data/**/page-data.json`)
+  console.log('[onPostBuild] Renaming the following files:')
+  for (const file of jsonFiles) {
+    console.log(file)
+    const newFilename = file.replace('page-data.json', `page-data.${hash}.json`)
+    await fs.renameSync(file, newFilename)
+  }
+  const appShaFiles = glob.sync(`${publicPath}/**/app-*.js`)
+  const [appShaFile] = appShaFiles
+  const [appShaFilename] = appShaFile.split('/').slice(-1)
+  const appShaFilenameReg = new RegExp(appShaFilename, 'g')
+  const newAppShaFilename = `app-${hash}.js`
+  const newFilePath = appShaFile.replace(appShaFilename, newAppShaFilename)
+  console.log(`[onPostBuild] Renaming: ${appShaFilename} to ${newAppShaFilename}`)
+  await fs.renameSync(appShaFile, newFilePath)
+  await fs.renameSync(`${appShaFile}.map`, `${newFilePath}.map`)
+  const htmlJSAndJSONFiles = [
+    `${newFilePath}.map`,
+    ...glob.sync(`${publicPath}/**/*.{html,js,json}`)
+  ]
+  console.log(
+    `[onPostBuild] Replacing page-data.json, ${appShaFilename}, and ${appShaFilename}.map references in the following files:`
+  )
+  for (const file of htmlJSAndJSONFiles) {
+    const stats = await fs.statSync(file, 'utf8')
+    if (!stats.isFile()) {
+      continue
+    }
+    const content = await fs.readFileSync(file, 'utf8')
+    const result = content
+      .replace(appShaFilenameReg, newAppShaFilename)
+      .replace(/page-data.json/g, `page-data.${hash}.json`)
+    if (result !== content) {
+      console.log(file)
+      await fs.writeFileSync(file, result, 'utf8')
+    }
+  }
 }
