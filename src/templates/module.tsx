396a5cd79f2176275de2e4d6a2ec8b64a6ebf987
Riko Sakurauchi <lijiahao99131@gmail.com>
2021-02-12 23:01:27 +0800

    :sparkles: add releases

--- src/templates/module.tsx
+++ src/templates/module.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Layout from '../layout'
 import * as React from 'react'
 import { graphql } from 'gatsby'
@@ -6,10 +6,13 @@ import SEO from '../components/seo'
 import { Grid } from '@material-ui/core'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
-const useStyles = makeStyles((_: Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      margin: '30px 0'
+      margin: '30px 0',
+      [theme.breakpoints.down('sm')]: {
+        margin: '10px 0'
+      }
     },
     document: {
       wordBreak: 'break-word'
@@ -35,12 +38,18 @@ const useStyles = makeStyles((_: Theme) =>
       '&:last-child': {
         marginBottom: 0
       }
+    },
+    release: {
+      [theme.breakpoints.down('sm')]: {
+        display: 'none'
+      }
     }
   })
 )
 
 export default function ModulePage ({ data }: any): ReactElement {
   const classes = useStyles()
+  const [showReleaseNum, setShowReleaseNum] = useState(1)
   return (
     <Layout>
       <SEO title={data.githubRepository.description} />
@@ -117,8 +126,77 @@ export default function ModulePage ({ data }: any): ReactElement {
               </div>)
               : ''
             }
+            {data.githubRepository.releases?.edges.length
+              ? (<div className={`${classes.box} ${classes.release}`}>
+                <h2 className={classes.h2}>Releases</h2>
+                <h3 className={classes.h2}>
+                  <a href={data.githubRepository.releases.edges[0].node.url}
+                     target={'_blank'}
+                  >{data.githubRepository.releases.edges[0].node.name}</a>
+                </h3>
+                <p className={classes.p}>
+                  Release Type: {data.githubRepository.releases.edges[0].node.isPrerelease ? 'Pre-release' : 'Stable'}
+                </p>
+                <p className={classes.p}>
+                  {new Date(data.githubRepository.releases.edges[0].node.publishedAt).toLocaleString()}
+                </p>
+                <p className={classes.p}>
+                  <a href={'#releases'}>View all releases</a>
+                </p>
+              </div>)
+              : ''
+            }
           </div>
         </Grid>
+        {data.githubRepository.releases?.edges.length
+          ? (<Grid item xs={12}>
+            <h1 id="releases">Releases</h1>
+            {data.githubRepository.releases.edges.slice(0, showReleaseNum).map(({ node: release }: any) => (
+              <div key={release.name}>
+                <h2><a href={release.url} target={'_blank'}>{release.name}</a></h2>
+                <p className={classes.p}>
+                  Release Type: {release.isPrerelease ? 'Pre-release' : 'Stable'}
+                </p>
+                <p className={classes.p}>
+                  {new Date(data.githubRepository.releases.edges[0].node.publishedAt).toLocaleString()}
+                </p>
+                <div
+                  className={classes.document}
+                  dangerouslySetInnerHTML={{
+                    __html: release.descriptionHTML
+                  }}
+                />
+                {release.releaseAssets?.edges.length
+                  ? (
+                    <div>
+                      <h3>Downloads</h3>
+                      <ul>
+                        {release.releaseAssets.edges.map(({ node: asset }: any) => (
+                          <li key={asset.name}>
+                            <a href={asset.downloadUrl} target={'_blank'}>{asset.name}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>)
+                  : ''
+                }
+              </div>
+            ))}
+            {showReleaseNum !== data.githubRepository.releases.edges.length
+              ? (<p>
+                <a
+                  href=""
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setShowReleaseNum(showReleaseNum + 1)
+                  }}
+                >Show older versions</a>
+              </p>)
+              : ''
+            }
+          </Grid>)
+          : ''
+        }
       </Grid>
     </Layout>
   )
@@ -151,6 +229,30 @@ query ($name: String!) {
     childGitHubReadme {
       childMarkdownRemark {
         html
+      }
+    }
+    releases {
+      edges {
+        node {
+          name
+          url
+          description
+          descriptionHTML
+          createdAt
+          publishedAt
+          updatedAt
+          tagName
+          isPrerelease
+          releaseAssets {
+            edges {
+              node {
+                name
+                contentType
+                downloadUrl
+              }
+            }
+          }
+        }
       }
     }
   }
