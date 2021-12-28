d0a6ae271807cce07410d0c9217af00e4087247d
Riko Sakurauchi <i@riko.love>
2021-12-28 00:51:29 -0500

    :bug: fix cache

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,6 @@ const fs = require('fs')
 const uuid = require('uuid')
 const crypto = require('crypto')
 const path = require('path')
-const glob = require('glob')
-const md5 = require('uuid')
 const fetch = require('node-fetch')
 
 const { fetchFromGithub } = require('./github-source')
@@ -430,53 +428,4 @@ exports.onPostBuild = async ({ graphql }) => {
     if (!repo.readmeHTML) repo.readmeHTML = repo.childGitHubReadme.childMarkdownRemark.html
   }
   fs.writeFileSync(`${rootPath}/modules.json`, JSON.stringify(modules))
-
-  // Work around for caches on gh-pages
-  // https://github.com/gatsbyjs/gatsby/issues/15080#issuecomment-765338035
-  const publicPath = path.join(__dirname, 'public')
-  const hash = md5(Math.random().toString(36).substring(7))
-  const jsonFiles = glob.sync(`${publicPath}/page-data/**/page-data.json`)
-  console.log(`[onPostBuild] Renaming the following files to page-data.${hash}.json:`)
-  for (const file of jsonFiles) {
-    console.log(file)
-    const newFilename = file.replace('page-data.json', `page-data.${hash}.json`)
-    // Renaming makes cache fails, so copy instead
-    fs.copyFileSync(file, newFilename)
-  }
-  const appShaFiles = glob.sync(`${publicPath}/**/app-+([^-]).js`)
-  const [appShaFile] = appShaFiles
-  const [appShaFilename] = appShaFile.split('/').slice(-1)
-  const appShaFilenameReg = new RegExp(appShaFilename, 'g')
-  const newAppShaFilename = `app-${hash}.js`
-  const newFilePath = appShaFile.replace(appShaFilename, newAppShaFilename)
-  console.log(`[onPostBuild] Copying: ${appShaFilename} to ${newAppShaFilename}`)
-  // Renaming makes cache fails, so copy instead
-  fs.copyFileSync(appShaFile, newFilePath)
-  if (fs.existsSync(`${appShaFile}.map`)) {
-    fs.copyFileSync(`${appShaFile}.map`, `${newFilePath}.map`)
-  }
-  if (fs.existsSync(`${appShaFile}.LICENSE.txt`)) {
-    fs.copyFileSync(`${appShaFile}.LICENSE.txt`, `${newFilePath}.LICENSE.txt`)
-  }
-  const htmlJSAndJSONFiles = [
-    `${newFilePath}.map`,
-    ...glob.sync(`${publicPath}/**/*.{html,js,json}`)
-  ]
-  console.log(
-    `[onPostBuild] Replacing page-data.json, ${appShaFilename}, and ${appShaFilename}.map references in the following files:`
-  )
-  for (const file of htmlJSAndJSONFiles) {
-    const stats = fs.statSync(file, 'utf8')
-    if (!stats.isFile()) {
-      continue
-    }
-    const content = fs.readFileSync(file, 'utf8')
-    const result = content
-      .replace(appShaFilenameReg, newAppShaFilename)
-      .replace(/page-data.json/g, `page-data.${hash}.json`)
-    if (result !== content) {
-      console.log(file)
-      fs.writeFileSync(file, result, 'utf8')
-    }
-  }
 }
