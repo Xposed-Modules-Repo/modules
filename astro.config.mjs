2a3434d9ebff231da254154915ef0354485dff01
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2025-06-18 22:40:05 +0800

    add: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
