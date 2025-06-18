f29fb4beca0b166a6d642e87932e9444765f8aea
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2025-06-18 22:46:39 +0800

    config(astro): set site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://modules.lsposed.org',
+
   integrations: [sitemap()],
 
   vite: {
