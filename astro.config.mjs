f5d181c2585dcede72af5bbe529657e1664de96c
Js0n <29531167+JasonKhew96@users.noreply.github.com>
2025-06-18 22:39:25 +0800

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap()]
+});
