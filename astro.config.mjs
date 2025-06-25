ee0c1757c9adaa86cff471fada7c659e9eb27d5c
Js0n <contact@jasonkhew96.dev>
2025-06-25 16:20:28 +0800

    feat: prefetch all

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,9 @@ import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
+  prefetch: {
+    prefetchAll: true
+  }
   site: 'https://modules.lsposed.org',
 
   integrations: [
