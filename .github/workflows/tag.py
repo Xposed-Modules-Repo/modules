0b614d0db947b31f07eb5b0211916df076da0d41
Js0n <contact@jasonkhew96.dev>
2026-08-25 07:05:45 +0000

    fix: config again

--- .github/workflows/tag.py
+++ .github/workflows/tag.py
@@ -42,8 +42,8 @@ def main():
     tag_name = f"{versionCode}-{versionName}"
 
     repo = git.Repo.init("./temp.git")
-    repo.config_writer("global").set_value("user", "name", "github-actions[bot]")
-    repo.config_writer("global").set_value("user", "email", "41898282+github-actions[bot]@users.noreply.github.com")
+    repo.config_writer("repository").set_value("user", "name", "github-actions[bot]")
+    repo.config_writer("repository").set_value("user", "email", "41898282+github-actions[bot]@users.noreply.github.com")
     repo.index.commit(tag_name)
     repo.create_tag(tag_name, message=tag_name, force=True)
     remote = repo.create_remote("origin", f"https://{tag_token}@github.com/{org_repo}.git")
