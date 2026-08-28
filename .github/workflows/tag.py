0222e927a2b3f835d7224eecff04a6b10e76e3f2
Js0n <contact@jasonkhew96.dev>
2026-08-28 13:52:20 +0000

    feat: draft bad release

--- .github/workflows/tag.py
+++ .github/workflows/tag.py
@@ -8,14 +8,26 @@ from os import getenv
 import git
 
 
+def update_release(bearer_token, org_repo, release_id, data):
+    resp = requests.patch(
+        f"https://api.github.com/repos/{org_repo}/releases/{release_id}",
+        json=data,
+        headers={
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": f"Bearer {bearer_token}",
+        },
+    )
+    resp.raise_for_status()
+
+
 def main():
     org_repo = getenv("REPO")
     release = getenv("RELEASE")
     apk = getenv("APK")
     tag_name = getenv("TAG")
-    tag_token = getenv("TAG_TOKEN")
+    bearer_token = getenv("TAG_TOKEN")
 
-    if not tag_token:
+    if not bearer_token:
         raise RuntimeError("TAG_TOKEN is missing")
 
     with requests.get(apk, stream=True, timeout=10) as r:
@@ -37,17 +49,19 @@ def main():
                         versionCode = p.androidversion["Code"]
                         versionName = p.androidversion["Name"]
                 else:
-                    raise RuntimeError("apk is not a Xposed module")
+                    update_release(bearer_token, org_repo, release, {"draft": True})
+                    return
 
     repo_name = org_repo if "/" not in org_repo else org_repo.split("/")[1]
     if package != repo_name:
-        raise RuntimeError(f"package name mismatched {package} != {repo_name}")
+        update_release(bearer_token, org_repo, release, {"draft": True})
+        return
 
     tag_name = f"{versionCode}-{versionName}"
 
     repo = git.Repo.init("./temp.git")
     remote = repo.create_remote(
-        "origin", f"https://{tag_token}@github.com/{org_repo}.git"
+        "origin", f"https://{bearer_token}@github.com/{org_repo}.git"
     )
     with repo.config_writer("repository") as w:
         w.set_value("user", "name", "github-actions[bot]")
@@ -58,16 +72,7 @@ def main():
     repo.create_tag(tag_name, message=tag_name, force=True)
     remote.push(tag_name, force=True)
 
-    resp = requests.patch(
-        f"https://api.github.com/repos/{org_repo}/releases/{release}",
-        json={"tag_name": tag_name},
-        headers={
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"Bearer {tag_token}",
-        },
-        timeout=10,
-    )
-    resp.raise_for_status()
+    update_release(bearer_token, org_repo, release, data={"tag_name": tag_name})
 
 
 if __name__ == "__main__":
