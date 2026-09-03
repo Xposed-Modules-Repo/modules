58649a5d1a0515fb531abfb949472f137ce03744
Js0n <contact@jasonkhew96.dev>
2026-09-03 06:56:20 +0000

    feat: print error compatible to github actions

--- .github/workflows/tag.py
+++ .github/workflows/tag.py
@@ -1,15 +1,25 @@
-from builtins import RuntimeError
-
-import requests
-from io import BytesIO
-from zipfile import ZipFile
 from axml.axml import AXMLPrinter
+from builtins import RuntimeError
+from io import BytesIO
 from os import getenv
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+from zipfile import ZipFile
+from sys import stdout
+
 import git
+import os
+import re
 
 
-def update_release(bearer_token, org_repo, release_id, data):
-    resp = requests.patch(
+def escape_data(text: str):
+    text = re.sub(r'%', '%25', text)
+    text = re.sub(r'\r', '%0D', text)
+    return re.sub(r'\n', '%0A', text)
+
+def update_release(session: Session, bearer_token, org_repo, release_id, data):
+    resp = session.patch(
         f"https://api.github.com/repos/{org_repo}/releases/{release_id}",
         json=data,
         headers={
@@ -17,8 +27,9 @@ def update_release(bearer_token, org_repo, release_id, data):
             "Authorization": f"Bearer {bearer_token}",
         },
     )
-    resp.raise_for_status()
-
+    if not resp.ok:
+        stdout.write(f"::error::{escape_data(resp.text)}")
+        os._exit(1)
 
 def main():
     org_repo = getenv("REPO")
@@ -30,10 +41,21 @@ def main():
     if not bearer_token:
         raise RuntimeError("TAG_TOKEN is missing")
 
-    with requests.get(apk, stream=True, timeout=10) as r:
+    session = Session()
+    retries = Retry(
+        total=3,
+        backoff_factor=1,
+        status_forcelist=[502, 503, 504],
+        allowed_methods=["GET", "PUT", "DELETE", "PATCH"]
+    )
+    session.mount('https://', HTTPAdapter(max_retries=retries))
+
+    with session.get(apk, stream=True, timeout=10) as r:
         if r.status_code == 404 or r.status_code == 422:
             return
-        r.raise_for_status()
+        if not r.ok:
+            stdout.write(f"::error::{escape_data(r.text)}")
+            os._exit(1)
         with BytesIO(r.content) as zip_buffer:
             with ZipFile(zip_buffer) as zf:
                 fn = "AndroidManifest.xml"
@@ -49,12 +71,16 @@ def main():
                         versionCode = p.androidversion["Code"]
                         versionName = "_".join(p.androidversion["Name"].split())
                 else:
-                    update_release(bearer_token, org_repo, release, {"draft": True})
+                    update_release(session, bearer_token, org_repo, release, {"draft": True})
+                    stdout.write(f"::error::{escape_data('apk is not a xposed module')}")
+                    os._exit(1)
                     return
 
     repo_name = org_repo if "/" not in org_repo else org_repo.split("/")[1]
     if package != repo_name:
-        update_release(bearer_token, org_repo, release, {"draft": True})
+        update_release(session, bearer_token, org_repo, release, {"draft": True})
+        stdout.write(f"::error::{escape_data('application id mismatched')}")
+        os._exit(1)
         return
 
     tag_name = f"{versionCode}-{versionName}"
@@ -72,7 +98,7 @@ def main():
     repo.create_tag(tag_name, message=tag_name, force=True)
     remote.push(tag_name, force=True)
 
-    update_release(bearer_token, org_repo, release, data={"tag_name": tag_name})
+    update_release(session, bearer_token, org_repo, release, data={"tag_name": tag_name})
 
 
 if __name__ == "__main__":
